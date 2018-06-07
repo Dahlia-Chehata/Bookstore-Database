5640c8d14806375f8a1cc305e4cfcd7cbeb25ccc
Salma Hesham Mohammed <salma.ragab153@gmail.com>
2018-06-07 16:59:18 +0200

    result view

--- BookStrore/src/gui/books_handling/ResultView.java
+++ BookStrore/src/gui/books_handling/ResultView.java
@@ -3,32 +3,38 @@ package gui.books_handling;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
 
 public class ResultView {
 
 	private JFrame frame;
+	private JTable table;
+	private String[] column_names;
+	private String[][] data;
+	private int total_size_of_data;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ResultView window = new ResultView();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	/**
+//	 * Launch the application.
+//	 */
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					ResultView window = new ResultView();
+//					window.frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the application.
 	 */
 	public ResultView() {
 		initialize();
+		this.frame.setVisible(true);
 	}
 
 	/**
@@ -38,6 +44,10 @@ public class ResultView {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
 
+		table = new JTable(arg0, arg1)
+		table.setBounds(419, 246, -415, -234);
+		frame.getContentPane().add(table);
+	}
 }
